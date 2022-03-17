faaf667f85a01717ad4aa597f5ebee5b8cac8c0c
MarcellGranat <granatcellmar98@gmail.com>
2022-03-17 21:50:23 +0100

    Update eov.R

--- codes/eov.R
+++ codes/eov.R
@@ -1,46 +1,59 @@
 library(tidyverse)
 library(rvest)
 
-read_html("http://service.psoft.hu/eovwgs100.php?eovy=637254&eovx=235286&jsCallback=EOVWGS.callback")
+highway_df <- readxl::read_excel("data/20211125-ap-au_csomópontok.xls") %>% # FIXME
+  janitor::row_to_names(1) %>%
+  janitor::clean_names() %>%
+  select(knev, x = keovx, y = keovy) %>%
+  mutate_at(-1, as.numeric) %>%
+  mutate_at(-1, round) %>%
+  mutate_at(-1, as.character) %>%
+  na.omit()
 
-eov <- function(eovy, eovx) {
-j <- read_html(str_c("http://service.psoft.hu/eovwgs100.php?eovy=", eovy, "&eovx=", eovx, "&jsCallback=EOVWGS.callback"))
-j
-lat <- as.character(j) %>% 
-  gsub(pattern = ".*lat", replacement = "") %>% 
-  gsub(pattern = ",.*", replacement = "") %>% 
-  gsub(pattern = '\":', replacement = "") %>% 
-  as.numeric()
+n_highway_done <- list.files("data/") %>% 
+  keep(str_detect, "highway_eov")
 
-lon <- as.character(j) %>% 
-  gsub(pattern = ".*lon", replacement = "") %>% 
-  gsub(pattern = ",.*", replacement = "") %>% 
-  gsub(pattern = '\":', replacement = "") %>% 
-  as.numeric()
+highway_eov_done <- list.files("data/", full.names = TRUE) %>% 
+  keep(str_detect, "highway_eov") %>% 
+  map(read_rds) %>% 
+  bind_rows()
 
-tibble(lat, lon)
+if (nrow(highway_eov_done) == 0) {
+  highway_eov_left <- highway_df
+} else {
+  highway_eov_left <- highway_df %>% 
+    anti_join(highway_eov_done)
 }
 
-df <- tibble::tribble(
-                                                                                                                      ~knev,       ~x,       ~y,
-                                                                             "M0 autóút körgyűrű /M1 - 11 sz. főút között/", "637254", "235286",
-                                                                             "M0 autóút körgyűrű /M1 - 11 sz. főút között/", "638146", "233408",
-                                                                             "M0 autóút körgyűrű /M1 - 11 sz. főút között/", "638501", "231997",
-                                                                             "M0 autóút körgyűrű /M1 - 11 sz. főút között/", "643304", "229831",
-                                                                             "M0 autóút körgyűrű /M1 - 11 sz. főút között/", "644656", "228464",
-                                                                             "M0 autóút körgyűrű /M1 - 11 sz. főút között/", "647242", "228193"
-                                                                             )
-# df <- readxl::read_excel("data/20211125-ap-au_csomópontok.xls") %>% # FIXME
-#   janitor::row_to_names(1) %>% 
-#   janitor::clean_names() %>% 
-#   select(knev, x = keovx, y = keovy) %>% 
-#   mutate_at(-1, as.numeric) %>% 
-#   mutate_at(-1, round) %>% 
-#   mutate_at(-1, as.character) %>% 
-#   na.omit() %>% 
-#   head() %>% 
-df %>% 
-  mutate(data = map2(x, y, eov)) %>% 
-  unnest()
+i <- 1
+eov_l <- list()
+
+for (i in 1:50) {
+  
+  eovy <- highway_eov_left$x[i]
+  eovx <- highway_eov_left$y[i]
+  
+  j <- read_html(str_c("http://service.psoft.hu/eovwgs100.php?eovy=", eovy, "&eovx=", eovx, "&jsCallback=EOVWGS.callback"))
+  if (str_detect(as.character(j), "Request limit exceeded")) break
+  lat <- as.character(j) %>% 
+    gsub(pattern = ".*lat", replacement = "") %>% 
+    gsub(pattern = ",.*", replacement = "") %>% 
+    gsub(pattern = '\":', replacement = "") %>% 
+    as.numeric()
+  
+  lon <- as.character(j) %>% 
+    gsub(pattern = ".*lon", replacement = "") %>% 
+    gsub(pattern = ",.*", replacement = "") %>% 
+    gsub(pattern = '\":', replacement = "") %>% 
+    as.numeric()
+  
+  eov_l[[i]] <- tibble(lat, lon)
+
+}
+
+eov_l %>% 
+  bind_rows() %>% 
+  saveRDS(file = str_c("data/highway_eov", n_highway_done + 1, ".rds"))
+
 
 
